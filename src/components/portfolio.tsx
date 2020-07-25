dc8399181293dc9550c23b1aaed5993e7bc5e291
DarylSerrano <darylserrano39@gmail.com>
2020-07-25 14:19:06 +0200

    Update portfolio with information

--- src/components/portfolio.tsx
+++ src/components/portfolio.tsx
@@ -9,40 +9,69 @@ import {
 import "react-vertical-timeline-component/style.min.css"
 import { MdWork } from "react-icons/md"
 
+type PortfolioQuery = {
+  allMarkdownRemark: {
+    edges: [
+      {
+        node: {
+          html: string
+          frontmatter: {
+            company: string
+            position: string
+            startDate: string
+            endDate: string
+          }
+        }
+      }
+    ]
+  }
+}
+
 export default function Portfolio() {
-  const { allPortfolioDataJson } = useStaticQuery(graphql`
+  const { allMarkdownRemark }: PortfolioQuery = useStaticQuery(graphql`
     query {
-      allPortfolioDataJson {
-        nodes {
-          company
-          description
-          job
+      allMarkdownRemark(
+        filter: { frontmatter: { category: { eq: "experiences" } } }
+        sort: { fields: frontmatter___date, order: DESC }
+      ) {
+        edges {
+          node {
+            id
+            html
+            frontmatter {
+              company
+              position
+              startDate
+              endDate
+              date(formatString: "MMMM DD, YYYY")
+            }
+          }
         }
       }
     }
   `)
 
-  console.log(allPortfolioDataJson)
+  console.log(allMarkdownRemark)
 
   return (
-    <Jumbotron style={{ height: "100vh" }}>
+    <Jumbotron>
       <Container fluid>
         <h2 className="jumbotron-heading">Job experience</h2>
-        <VerticalTimeline layout="1-column">
-          {allPortfolioDataJson.nodes.map(jobInfo => {
+        <VerticalTimeline animate={false} layout="1-column">
+          {allMarkdownRemark.edges.map(edge => {
             return (
               <VerticalTimelineElement
                 className="vertical-timeline-element--work"
-                date="2011 - present"
+                date={`${edge.node.frontmatter.startDate} - ${edge.node.frontmatter.endDate}`}
                 icon={<MdWork />}
               >
                 <h3 className="vertical-timeline-element-title">
-                  {jobInfo.job}
+                  {edge.node.frontmatter.position}
                 </h3>
                 <h4 className="vertical-timeline-element-subtitle">
-                  {jobInfo.company}
+                  {edge.node.frontmatter.company}
                 </h4>
-                <p>{jobInfo.description}</p>
+                <div dangerouslySetInnerHTML={{ __html: edge.node.html }}></div>
               </VerticalTimelineElement>
             )
           })}
